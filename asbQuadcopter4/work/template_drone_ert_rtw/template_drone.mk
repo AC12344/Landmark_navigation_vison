91ffa789f7bdcf70e80ea801db32baf845ac0c9d
Alexander Sokolovs <alsok20@student.sdu.dk>
2021-06-01 16:46:49 +0200

    Drone flies :D but wants to hug the ceiling after some seconds...

--- asbQuadcopter4/work/template_drone_ert_rtw/template_drone.mk
+++ asbQuadcopter4/work/template_drone_ert_rtw/template_drone.mk
@@ -2,7 +2,7 @@
 ## Makefile generated for component 'template_drone'. 
 ## 
 ## Makefile     : template_drone.mk
-## Generated on : Tue Jun 01 15:50:20 2021
+## Generated on : Tue Jun 01 16:30:59 2021
 ## Final product: $(RELATIVE_PATH_TO_ANCHOR)/template_drone.so
 ## Product type : executable
 ## 
