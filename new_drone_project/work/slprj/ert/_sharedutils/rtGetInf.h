2a73dee0d998e78eac646005d25a68252bfebf25
Alexander Sokolovs <alsok20@student.sdu.dk>
2021-06-01 13:08:04 +0200

    Fixed some more errors but error: The component 'Coder Target (CoderTarget.SettingsController)' of the model 'template_drone' does not exist for the model 'stateEstimator' still remains. Going step by step now.

---
+++ new_drone_project/work/slprj/ert/_sharedutils/rtGetInf.h
@@ -0,0 +1,32 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: rtGetInf.h
+ *
+ * Code generated for Simulink model 'stateEstimator'.
+ *
+ * Model version                  : 1.46
+ * Simulink Coder version         : 9.4 (R2020b) 29-Jul-2020
+ * C/C++ source code generated on : Tue Jun  1 13:04:23 2021
+ */
+
+#ifndef RTW_HEADER_rtGetInf_h_
+#define RTW_HEADER_rtGetInf_h_
+#include <stddef.h>
+#include "rtwtypes.h"
+#include "rt_nonfinite.h"
+
+extern real_T rtGetInf(void);
+extern real32_T rtGetInfF(void);
+extern real_T rtGetMinusInf(void);
+extern real32_T rtGetMinusInfF(void);
+
+#endif                                 /* RTW_HEADER_rtGetInf_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
--- new_drone_project/work/slprj/ert/_sharedutils/rtwtypes.h
+++ new_drone_project/work/slprj/ert/_sharedutils/rtwtypes.h
@@ -1,3 +1,17 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: rtwtypes.h
+ *
+ * Code generated for Simulink model 'stateEstimator'.
+ *
+ * Model version                  : 1.46
+ * Simulink Coder version         : 9.4 (R2020b) 29-Jul-2020
+ * C/C++ source code generated on : Tue Jun  1 13:04:23 2021
+ */
+
 #ifndef RTWTYPES_H
 #define RTWTYPES_H
 
