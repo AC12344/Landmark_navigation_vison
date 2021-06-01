02e25ddab5225cb9e1bdd6f8c0f8cb915cb04ab9
Alexander Sokolovs <alsok20@student.sdu.dk>
2021-06-01 13:22:23 +0200

    clearly stateEstimator and controller are the problem

--- new_drone_project/work/slprj/ert/_sharedutils/const_params.c
+++ new_drone_project/work/slprj/ert/_sharedutils/const_params.c
@@ -5,17 +5,22 @@
  * course requirements at degree granting institutions only.  Not for
  * government, commercial, or other organizational use.
  *
- * Code generation for model "stateEstimator".
+ * Code generation for model "flightController".
  *
- * Model version              : 1.46
+ * Model version              : 1.273
  * Simulink Coder version : 9.4 (R2020b) 29-Jul-2020
- * C source code generated on : Tue Jun  1 13:04:23 2021
+ * C source code generated on : Tue Jun  1 13:21:09 2021
  */
 #include "rtwtypes.h"
 
 extern const real32_T rtCP_pooled_2aITfOsIWEfl[6];
 const real32_T rtCP_pooled_2aITfOsIWEfl[6] = { 0.994075298F, 0.996184587F,
   1.00549F, 1.00139189F, 0.993601203F, 1.00003F } ;
+
+extern const real32_T rtCP_pooled_AiPFoGkd3zrs[16];
+const real32_T rtCP_pooled_AiPFoGkd3zrs[16] = { 0.25F, 0.25F, 0.25F, 0.25F,
+  103.573624F, -103.573624F, 103.573624F, -103.573624F, -5.66592F, -5.66592F,
+  5.66592F, 5.66592F, -5.66592F, 5.66592F, 5.66592F, -5.66592F } ;
 
 extern const real32_T rtCP_pooled_KvfM2cKww6Wz[6];
 const real32_T rtCP_pooled_KvfM2cKww6Wz[6] = { 0.0264077242F, 0.140531361F,
@@ -44,3 +49,6 @@ const real32_T rtCP_pooled_bW9qz7zDL7dr[4] = { 0.0F, 0.0F, 0.0F, 0.0F } ;
 
 extern const real32_T rtCP_pooled_kqUvCpwgnPyl[4];
 const real32_T rtCP_pooled_kqUvCpwgnPyl[4] = { 1.0F, 0.0F, 0.0F, 1.0F } ;
+
+extern const real32_T rtCP_pooled_oEBofkCxx3u4[4];
+const real32_T rtCP_pooled_oEBofkCxx3u4[4] = { 1.0F, -1.0F, 1.0F, -1.0F } ;
---
+++ new_drone_project/work/slprj/ert/flightController/html/define.js
@@ -0,0 +1,87 @@
+function CodeDefine() { 
+this.def = new Array();
+this.def["flightController_MdlrefDW"] = {file: "flightController_c.html",line:23,type:"var"};
+this.def["flightController_DW"] = {file: "flightController_c.html",line:26,type:"var"};
+this.def["flightController_Init"] = {file: "flightController_c.html",line:29,type:"fcn"};
+this.def["flightController_run"] = {file: "flightController_c.html",line:38,type:"fcn"};
+this.def["flightController_initialize"] = {file: "flightController_c.html",line:421,type:"fcn"};
+this.def["DW_flightController_f_T"] = {file: "flightController_h.html",line:39,type:"type"};
+this.def["MdlrefDW_flightController_T"] = {file: "flightController_h.html",line:56,type:"type"};
+this.def["CommandBus"] = {file: "flightController_types_h.html",line:34,type:"type"};
+this.def["statesEstim_t"] = {file: "flightController_types_h.html",line:54,type:"type"};
+this.def["RT_MODEL_flightController_T"] = {file: "flightController_types_h.html",line:59,type:"type"};
+this.def["rtCP_pooled_2aITfOsIWEfl"] = {file: "../../_sharedutils/html/const_params_c.html",line:17,type:"var"};
+this.def["rtCP_pooled_AiPFoGkd3zrs"] = {file: "../../_sharedutils/html/const_params_c.html",line:21,type:"var"};
+this.def["rtCP_pooled_KvfM2cKww6Wz"] = {file: "../../_sharedutils/html/const_params_c.html",line:26,type:"var"};
+this.def["rtCP_pooled_OlAt8KbpQEG0"] = {file: "../../_sharedutils/html/const_params_c.html",line:30,type:"var"};
+this.def["rtCP_pooled_VH0dmwbCQMUq"] = {file: "../../_sharedutils/html/const_params_c.html",line:33,type:"var"};
+this.def["rtCP_pooled_aLwfmFKBoRwh"] = {file: "../../_sharedutils/html/const_params_c.html",line:37,type:"var"};
+this.def["rtCP_pooled_adLCBuYxtU1Q"] = {file: "../../_sharedutils/html/const_params_c.html",line:41,type:"var"};
+this.def["rtCP_pooled_b5NceP1yVsZG"] = {file: "../../_sharedutils/html/const_params_c.html",line:45,type:"var"};
+this.def["rtCP_pooled_bW9qz7zDL7dr"] = {file: "../../_sharedutils/html/const_params_c.html",line:48,type:"var"};
+this.def["rtCP_pooled_kqUvCpwgnPyl"] = {file: "../../_sharedutils/html/const_params_c.html",line:51,type:"var"};
+this.def["rtCP_pooled_oEBofkCxx3u4"] = {file: "../../_sharedutils/html/const_params_c.html",line:54,type:"var"};
+this.def["int8_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:46,type:"type"};
+this.def["uint8_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:47,type:"type"};
+this.def["int16_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:48,type:"type"};
+this.def["uint16_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:49,type:"type"};
+this.def["int32_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:50,type:"type"};
+this.def["uint32_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:51,type:"type"};
+this.def["real32_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:52,type:"type"};
+this.def["real64_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:53,type:"type"};
+this.def["real_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:59,type:"type"};
+this.def["time_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:60,type:"type"};
+this.def["boolean_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:61,type:"type"};
+this.def["int_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:62,type:"type"};
+this.def["uint_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:63,type:"type"};
+this.def["ulong_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:64,type:"type"};
+this.def["char_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:65,type:"type"};
+this.def["uchar_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:66,type:"type"};
+this.def["byte_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:67,type:"type"};
+this.def["creal32_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:77,type:"type"};
+this.def["creal64_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:82,type:"type"};
+this.def["creal_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:87,type:"type"};
+this.def["cint8_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:94,type:"type"};
+this.def["cuint8_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:101,type:"type"};
+this.def["cint16_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:108,type:"type"};
+this.def["cuint16_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:115,type:"type"};
+this.def["cint32_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:122,type:"type"};
+this.def["cuint32_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:129,type:"type"};
+this.def["pointer_T"] = {file: "../../_sharedutils/html/rtwtypes_h.html",line:147,type:"type"};
+}
+CodeDefine.instance = new CodeDefine();
+var testHarnessInfo = {OwnerFileName: "", HarnessOwner: "", HarnessName: "", IsTestHarness: "0"};
+var relPathToBuildDir = "../ert_main.c";
+var fileSep = "\\";
+var isPC = true;
+function Html2SrcLink() {
+	this.html2SrcPath = new Array;
+	this.html2Root = new Array;
+	this.html2SrcPath["flightController_c.html"] = "../flightController.c";
+	this.html2Root["flightController_c.html"] = "flightController_c.html";
+	this.html2SrcPath["flightController_h.html"] = "../flightController.h";
+	this.html2Root["flightController_h.html"] = "flightController_h.html";
+	this.html2SrcPath["flightController_private_h.html"] = "../flightController_private.h";
+	this.html2Root["flightController_private_h.html"] = "flightController_private_h.html";
+	this.html2SrcPath["flightController_types_h.html"] = "../flightController_types.h";
+	this.html2Root["flightController_types_h.html"] = "flightController_types_h.html";
+	this.html2SrcPath["const_params_c.html"] = "../const_params.c";
+	this.html2Root["const_params_c.html"] = "../../_sharedutils/html/const_params_c.html";
+	this.html2SrcPath["rtwtypes_h.html"] = "../rtwtypes.h";
+	this.html2Root["rtwtypes_h.html"] = "../../_sharedutils/html/rtwtypes_h.html";
+	this.getLink2Src = function (htmlFileName) {
+		 if (this.html2SrcPath[htmlFileName])
+			 return this.html2SrcPath[htmlFileName];
+		 else
+			 return null;
+	}
+	this.getLinkFromRoot = function (htmlFileName) {
+		 if (this.html2Root[htmlFileName])
+			 return this.html2Root[htmlFileName];
+		 else
+			 return null;
+	}
+}
+Html2SrcLink.instance = new Html2SrcLink();
+var fileList = [
+"flightController_c.html","flightController_h.html","flightController_private_h.html","flightController_types_h.html","const_params_c.html","rtwtypes_h.html"];
